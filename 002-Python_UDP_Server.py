6e7d9977d28604477a20f61eaa87988cdb67634c
Yusuf ALTUN <68498379+yusufAltuun7@users.noreply.github.com>
2026-01-12 17:52:16 +0300

    Some changes.

--- 002-Python_UDP_Server.py
+++ 002-Python_UDP_Server.py
@@ -2,15 +2,18 @@ import socket
 from _socket import SOCK_DGRAM
 from idlelib.iomenu import encoding
 
+#AF_INET = IPv4
+#SOCK_DGRAM = UDP Socket
+
 sock = socket.socket(socket.AF_INET, socket. SOCK_DGRAM)
 sock.bind(('127.0.0.105', 12456))
 
 
 while True:
-    print('deneme')
+    print('First Attempt. Is it working?')
     data1, addr1 = sock.recvfrom(4096)
     data2, addr2 = sock.recvfrom(4096)
     print(str(data1))
     print(str(data2))
-    sock.sendto(str("120126").encode('ascii'),addr1)
+    sock.sendto(str("I'm sending this message from Server to Client").encode('ascii'),addr1)
     sock.sendto(str(data2).encode('ascii'),addr2)
--- 003-Python_UDP_Client.py
+++ 003-Python_UDP_Client.py
@@ -1,47 +1,21 @@
-'''
 import socket
 from _socket import SOCK_DGRAM
 from idlelib.iomenu import encoding
 from _overlapped import NULL
 
-msg = b'\x23' + 47 * b'\0'
+#AF_INET = IPv4
+#SOCK_DGRAM = UDP
+
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#msg1 = 'dere boyu kavaklar'
-#msg2 = 'acti yesil yapraklar'
-
-client_sock.sendto(msg, ("127.0.10.10", 12456))
-
-data1, addr1 = client_sock.recvfrom(4096)
-print("serverdan bana gelen cevap:")
-print(str(data1))
-
-#client_sock.sendto(msg2.encode(encoding="utf-8", errors="ignore"),('127.0.0.10', 12456))
-#print("kontrol 1")
-
-
-data1, addr1 = client_sock.recvfrom(4096)
-data2, addr2 = client_sock.recvfrom(4096)
-print("serverdan bana gelen cevap:")
-print(str(data1))
-print(str(data2))
-#print(str(data))
-
-client_sock.close()
-'''
-import socket
-from _socket import SOCK_DGRAM
-from idlelib.iomenu import encoding
-from _overlapped import NULL
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-msg1 = 'dere boyu kavaklar'
-msg2 = 'acti yesil yapraklar'
+msg1 = 'Message One: Sending through Client to Server'
+msg2 = 'Message Two: Sender is Yusuf ALTUN'
 client_sock.sendto(msg1.encode(encoding="utf-8", errors="ignore"),('127.0.0.105', 12456))
 client_sock.sendto(msg2.encode(encoding="utf-8", errors="ignore"),('127.0.0.105', 12456))
-#print("kontrol 1")
+
+
 data1, addr1 = client_sock.recvfrom(4096)
 data2, addr2 = client_sock.recvfrom(4096)
-print("serverdan bana gelen cevap:")
+print("Incoming messages from Server:")
 print(str(data1))
 print(str(data2))
 #print(str(data))
