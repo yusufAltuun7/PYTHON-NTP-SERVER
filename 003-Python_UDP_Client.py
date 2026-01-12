bb90e02a51c8f4c7fb4ac32421cf60059eae0882
Yusuf ALTUN <68498379+yusufAltuun7@users.noreply.github.com>
2026-01-12 17:40:08 +0300

    Add files via upload
    Fixing errors

--- 003-Python_UDP_Client.py
+++ 003-Python_UDP_Client.py
@@ -1,3 +1,4 @@
+'''
 import socket
 from _socket import SOCK_DGRAM
 from idlelib.iomenu import encoding
@@ -9,12 +10,12 @@ client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #msg1 = 'dere boyu kavaklar'
 #msg2 = 'acti yesil yapraklar'
 
-client_sock.sendto(msg, ("127.168.1.0", 123))
+client_sock.sendto(msg, ("127.0.10.10", 12456))
 
 data1, addr1 = client_sock.recvfrom(4096)
 print("serverdan bana gelen cevap:")
 print(str(data1))
-'''
+
 #client_sock.sendto(msg2.encode(encoding="utf-8", errors="ignore"),('127.0.0.10', 12456))
 #print("kontrol 1")
 
@@ -25,5 +26,23 @@ print("serverdan bana gelen cevap:")
 print(str(data1))
 print(str(data2))
 #print(str(data))
+
+client_sock.close()
 '''
+import socket
+from _socket import SOCK_DGRAM
+from idlelib.iomenu import encoding
+from _overlapped import NULL
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+msg1 = 'dere boyu kavaklar'
+msg2 = 'acti yesil yapraklar'
+client_sock.sendto(msg1.encode(encoding="utf-8", errors="ignore"),('127.0.0.105', 12456))
+client_sock.sendto(msg2.encode(encoding="utf-8", errors="ignore"),('127.0.0.105', 12456))
+#print("kontrol 1")
+data1, addr1 = client_sock.recvfrom(4096)
+data2, addr2 = client_sock.recvfrom(4096)
+print("serverdan bana gelen cevap:")
+print(str(data1))
+print(str(data2))
+#print(str(data))
 client_sock.close()
