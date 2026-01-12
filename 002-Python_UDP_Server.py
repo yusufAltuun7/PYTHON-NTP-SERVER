5ce9856739e9310fa2b4c6d0081a49695f69dbac
Yusuf ALTUN <68498379+yusufAltuun7@users.noreply.github.com>
2026-01-12 17:42:15 +0300

    Bugs Fixed

--- 002-Python_UDP_Server.py
+++ 002-Python_UDP_Server.py
@@ -3,7 +3,7 @@ from _socket import SOCK_DGRAM
 from idlelib.iomenu import encoding
 
 sock = socket.socket(socket.AF_INET, socket. SOCK_DGRAM)
-sock.bind(('127.0.0.10', 12456))
+sock.bind(('127.0.0.105', 12456))
 
 
 while True:
@@ -12,5 +12,5 @@ while True:
     data2, addr2 = sock.recvfrom(4096)
     print(str(data1))
     print(str(data2))
-    sock.sendto(str(data1).encode('ascii'),addr1)
+    sock.sendto(str("120126").encode('ascii'),addr1)
     sock.sendto(str(data2).encode('ascii'),addr2)
